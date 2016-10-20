af062b01e443213ef71bdfd4cabae5cfdfaaaae2
alan <alan@isend.pe>
2016-10-20 17:10:00 -0500

    set url parameters into an literal object

--- server.js
+++ server.js
@@ -8,12 +8,28 @@ var fs = require("fs");
 var manage_handler = function(req,res){
 	console.log("New Request");
 	var path = "./index.html";
+	var obParams = {};
 
 	//check if there are parameters in the url
 	if(req.url.indexOf("?") > 0){
-		console.log("url have parameters");
+		
+		//extract url parameters
+		console.log(req.url);
+		//regular expression for searching parameters in the url string
+		var regex = /[^\?&]+(?=&|\b)/g;
+		var params = req.url.match(regex);
+		console.log(params);
+		//set keys and values into obParams object
+		params.forEach(function(value){
+			var tmp = value.split("=");
+			obParams[tmp[0]] = tmp[1];
+		})
+		console.log(" obParams :" );
+		console.dir(obParams);
+
 	}
 
+	//reading the html file for write in the response
 	fs.readFile(path,function(err,html){
 		var data = html.toString();
 		res.writeHead(200,{'Content-Type':'text/html'});

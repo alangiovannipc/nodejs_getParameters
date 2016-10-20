e154f082e62e7d01079c38d3573148ddd382d920
alan <alan@isend.pe>
2016-10-20 12:30:23 -0500

    create the form and sending it in the response

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 //load http module
 var http = require("http");
 
+//load file system module;
+var fs = require("fs");
+
 //set a manage handler
 var manage_handler = function(req,res){
 	console.log("New Request");
-	res.writeHead(200,{'Content-Type':'text/html'});
-	res.end("Init Server");
+	var path = "./index.html";
+
+	fs.readFile(path,function(err,html){
+		var data = html.toString();
+		res.writeHead(200,{'Content-Type':'text/html'});
+		res.write(data);
+		res.end();
+	});
+	
 }
 
 //Created the server and passed the manage handler function

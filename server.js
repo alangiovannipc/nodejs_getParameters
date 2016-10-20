d90202c0055fac0ee4303793ee9de4a450932c47
alan <alan@isend.pe>
2016-10-20 12:45:23 -0500

    added detection url parameters

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var manage_handler = function(req,res){
 	console.log("New Request");
 	var path = "./index.html";
 
+	//check if there are parameters in the url
+	if(req.url.indexOf("?") > 0){
+		console.log("url have parameters");
+	}
+
 	fs.readFile(path,function(err,html){
 		var data = html.toString();
 		res.writeHead(200,{'Content-Type':'text/html'});
